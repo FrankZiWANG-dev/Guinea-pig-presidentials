f734ec1154c5b46da572f117174e18c467f512cc
FrankZiWANG-dev <wang.frank.zi@gmail.com>
2021-09-17 16:00:09 +0200

    most of home page

--- index.php
+++ index.php
@@ -18,16 +18,21 @@ include_once "nav.php";
 </head>
 <body>
 
-<h1>Time to elect a new Supreme Overlord !!</h1>
+<div id="home-container">
+    <img src="https://github.com/FrankZiWANG-dev/Guinea-pig-presidentials/blob/master/assets/images/home-logo.png?raw=true" alt="home-guinea-pig-logo" id="home-logo">
+    <div id="home-text">
+        <h1>Time to elect a new Supreme Overlord !!</h1>
 
-<p>The Revolution has come, humans !</p>
-<p>The guinea pigs have taken over “the Apartment” !</p>
-<p>Learn more about the circumstances of this glorious event and its participants, and vote!</p>
-<p>You may also contact the former master of these lands, who was charged with creating this website.</p>
+        <p>The Revolution has come, humans !</p>
 
-<p>Sincerely (not really) yours,</p>
-<p>The Guinea Pigs.</p>
+        <p>The guinea pigs have taken over “the Apartment” !</br>
+        Learn more about the circumstances of this glorious event and its participants, and vote!</br>
+        You may also contact the former master of these lands, who was charged with creating this website.</br>
 
+        <p>Sincerely (not really) yours,</br>
+        The Guinea Pigs.</p>
+    </div>
+</div>
 </body>
 </html>
 
