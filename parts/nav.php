ef39da05147cf4228497a8e2efa64b9c2dcdd201
FrankZiWANG-dev <wang.frank.zi@gmail.com>
2021-09-23 11:57:40 +0200

    add vote button

--- parts/nav.php
+++ parts/nav.php
@@ -5,11 +5,11 @@ echo "<nav>"
         ."<img src='https://github.com/FrankZiWANG-dev/Guinea-pig-presidentials/blob/master/assets/images/President-guinea-pig.jpg?raw=true' alt='president-guinea-pig' id='president-guinea-pig'>"
         ."<a href='' id='title-text'> Guinea pig presidentials! </a>"
         ."</div>"
-        ."<a href='' class='nav-link'> Home </a>"
-        ."<a href='' class='nav-link'> About the project </a>"
-        ."<a href='' class='nav-link'> Candidates </a>"
-        ."<a href='' class='nav-link'> Contact us </a>"
-        ."<a href='' id='nav-vote'> Vote! </a>"
+        ."<a href='index.php' class='nav-link'> Home </a>"
+        ."<a href='project.php' class='nav-link'> About the project </a>"
+        ."<a href='candidates.php' class='nav-link'> Candidates </a>"
+        ."<a href='contact.php' class='nav-link'> Contact us </a>"
+        ."<a href='vote.php' id='nav-vote'> Vote! </a>"
     ."</nav>";
 
 ?>
