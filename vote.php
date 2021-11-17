781519294dd2890d79a210d8dce88297070fe0eb
FrankZiWANG-dev <wang.frank.zi@gmail.com>
2021-11-17 16:22:38 +0100

    fixing db

--- vote.php
+++ vote.php
@@ -1,7 +1,7 @@
 <?php
 include_once "parts/nav.php";
 //Get Heroku ClearDB connection information
-$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+// $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 $cleardb_server = $cleardb_url["host"];
 $cleardb_username = $cleardb_url["user"];
 $cleardb_password = $cleardb_url["pass"];
